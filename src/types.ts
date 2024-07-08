2c7273cd3fbf7fe8aa9584a08cc148826f1c773f
D4rkr34lm <darth.m.frohn@gmail.com>
2024-07-08 22:25:12 +0200

    Implemented type factories

---
+++ src/types.ts
@@ -0,0 +1,82 @@
+import { isBoolean, isNull, isNumber, isString, toNumber } from "lodash";
+import * as moment from "moment";
+
+function noTransform<T>(value: T): T {
+  return value;
+}
+
+export function number() {
+  return {
+    type: "DOUBLE" as const,
+    validator: (value: unknown): value is number | null => {
+      return isNull(value) || isNumber(value);
+    },
+    fromDB: noTransform,
+    toDB: toNumber,
+  };
+}
+
+export function varChar(length: number) {
+  if (length < 1 || length > 255) {
+    throw `VarChars length has to be in [1,255] but ${length} was given`;
+  }
+
+  return {
+    type: `VARCHAR${length}` as const,
+    validator: (value: unknown): value is string | null => {
+      return isNull(value) || (isString(value) && value.length >= 0 && value.length < length);
+    },
+    fromDB: noTransform,
+    toDB: noTransform,
+  };
+}
+
+export function text() {
+  return {
+    type: "TEXT" as const,
+    validator: (value: unknown): value is string | null => {
+      return isNull(value) || isString(value);
+    },
+    fromDB: noTransform,
+    toDB: noTransform,
+  };
+}
+
+export function boolean() {
+  return {
+    type: "BOOLEAN" as const,
+    validator: (value: unknown): value is boolean | number => {
+      return isNull(value) || isBoolean(value);
+    },
+    fromDB: (value: any): boolean => {
+      return value as boolean;
+    },
+    toDB: noTransform,
+  };
+}
+
+export function timestamp() {
+  return {
+    type: "TIMESTAMPTZ" as const,
+    validator: (value: unknown): value is Date => {
+      return isString(value) && moment(value).isValid();
+    },
+    fromDB: (value: any) => {
+      return new Date(value);
+    },
+    toDB: (date: Date) => {
+      return date.toUTCString();
+    },
+  };
+}
+
+export const DBTypeFactories = {
+  number: number,
+  varChar: varChar,
+  text: text,
+  boolean: boolean,
+  timestamp: timestamp,
+};
+
+export type DBTypeName = keyof typeof DBTypeFactories;
+export type DBTypeFactory = (typeof DBTypeFactories)[DBTypeName];
